29982d95448bd9e515ba880b51a3878e0d0ada16
Prakriti Rawat <78833575+PrakritiRaw@users.noreply.github.com>
2022-11-21 16:26:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+#importing the libraries
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+#we created a data frame
+df=pd.read_csv('news.csv')
+
+print(df.shape)
+print(df.head())
+labels=df.label
+
+#we are splitting the dataset into test and train
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+
+#here we are initializing the tfidf vectorizer
+#the tfidf vectorizer uses a formula and puts the calculated values into a vector
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+tfidf_train=tfidf_vectorizer.fit_transform(x_train)
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+#training and testing the model, using the PAC
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+
+#RESULTS
+print(f'Accuracy: {round(score*100,2)}%')
+print("THE CONFUSION MATRIX:")
+print(confusion_matrix(y_test,y_pred, labels=['FAKE','REAL']))
+
